603580afcd3b6a7422e97291b297bcf06abb01f5
Ilya Groshev <groshev.i.d@gmail.com>
2018-08-25 18:42:48 +0300

    simplified contract

--- contracts/eoscanvas/eoscanvas.cpp
+++ contracts/eoscanvas/eoscanvas.cpp
@@ -1,55 +1,55 @@
 #include <eosiolib/eosio.hpp>
 
 using namespace eosio;
+class picture : public eosio::contract
+{
 
-class picture : public eosio::contract {
-
-private:
-
-    auto validateInput(char x, char y, char r,char  g,char  b) {
-        eosio_assert(x < picture_width, "X must be less then 100");
-        eosio_assert(y < picture_height, "X must be less then 100");
+  private:
+    uint32_t getXY(uint16_t x, uint16_t y)
+    {
+        // Первые 16 бит 32 битного числа отдаем под X координату, последние - под Y координату
+        return (uint32_t)(x << 16) + y;
     }
-public:
+    uint16_t getX(uint32_t xy)
+    {
+        return (uint16_t)(xy >> 16);
+    }
+    uint16_t getY(uint32_t xy)
+    {
+        return (uint16_t)(xy & 0xFFFF);
+    }
+
+  public:
     using contract::contract;
 
-    picture( account_name self ):contract(self), pixels(self, self){}
-    // параметры пикчи, мб пригодится
-    static const char picture_width = 5;
-    static const char picture_height = picture_width;
-    // Структура - описание пикселя
-    /// @abi table
-    struct pixel {
+    picture(account_name self) : contract(self), pixels(self, self) {}
 
+    /// @abi tablexw
+    struct pixel
+    {
         // ключ - для ячейки - ее порядковый номер, если считать слево-направо, сверху вниз
-        uint64_t primary_key() const { return key; };
         uint64_t key;
-        char X;
-        char Y;
-        char R = 0;
-        char G = 0;
-        char B = 0;
+        uint8_t r = 0;
+        uint8_t g = 0;
+        uint8_t b = 0;
 
-        EOSLIB_SERIALIZE(pixel,(key)(X)(Y)(R)(G)(B))
+        auto primary_key() const { return key; };
+        EOSLIB_SERIALIZE(pixel, (key)(r)(g)(b))
     };
-    // Объявим таблицу, хранящуюю пиксели
+
     typedef eosio::multi_index<N(pixel), pixel> pixelsstore;
     pixelsstore pixels;
 
     /// @abi action
-    void setpixel(char x, char y, char r, char g, char b) {
-
-        validateInput(x, y, r, g, b);
-        pixels.emplace(get_self(), [&](pixel& p)
-        {
-            p.key = y * picture_width + x;
-            p.X = x;
-            p.Y = y;
-            p.R = r;
-            p.G = g;
-            p.B = b;
+    void setpixel(char x, char y, char r, char g, char b)
+    {
+        pixels.emplace(get_self(), [&](pixel &p) {
+            p.key = getXY(x, y);
+            p.r = r;
+            p.g = g;
+            p.b = b;
         });
     }
 };
 
-EOSIO_ABI( picture, (setpixel))
+EOSIO_ABI(picture, (setpixel))
