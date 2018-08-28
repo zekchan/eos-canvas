cdede47ef768dcd0d7e96c574f078de7c5968291
Ilya Groshev <groshev.i.d@gmail.com>
2018-08-28 19:01:36 +0300

    pixel update and ownership

--- contracts/eoscanvas/eoscanvas.cpp
+++ contracts/eoscanvas/eoscanvas.cpp
@@ -26,25 +26,42 @@ class eoscanvas : public eosio::contract
         uint8_t r = 0;
         uint8_t g = 0;
         uint8_t b = 0;
+        account_name owner;
 
         auto primary_key() const { return key; };
-        EOSLIB_SERIALIZE(pixel, (key)(r)(g)(b))
+        EOSLIB_SERIALIZE(pixel, (key)(r)(g)(b)(owner))
     };
 
     typedef eosio::multi_index<N(pixel), pixel> pixelsstore;
     pixelsstore pixels;
 
     /// @abi action
-    void setpixel(uint32_t x, uint32_t y, uint8_t r, uint8_t g, uint8_t b)
+    void setpixel(account_name user, uint32_t x, uint32_t y, uint8_t r, uint8_t g, uint8_t b)
     {
-        pixels.emplace(get_self(), [&](pixel &p) {
-            p.key = getXY(x, y);
-            p.x = x;
-            p.y = y;
-            p.r = r;
-            p.g = g;
-            p.b = b;
-        });
+        require_auth(user);
+        auto key = getXY(x, y);
+        auto itr = pixels.find(key);
+        if (itr = pixels.end())
+        {
+            pixels.emplace(user, [&](pixel &p) {
+                p.key = getXY(x, y);
+                p.owner = user;
+                p.x = x;
+                p.y = y;
+                p.r = r;
+                p.g = g;
+                p.b = b;https://developers.eos.io/eosio-cpp/reference#modify
+            });
+        }
+        else
+        {
+            pixels.modify(itr, user, [&](auto &p) {
+                p.r = r;
+                p.owner = user;
+                p.g = g;
+                p.b = b;
+            })
+        }
     }
 };
 
